27b620d4a125a2cad73f3bc99d97fa50b3ca9da3
Reshord <daribayev1982@bk.ru>
2024-07-28 04:41:04 +0500

    commit reqUpdate

--- main.py
+++ main.py
@@ -182,5 +182,5 @@ async def generate_lesson_plan(request: LessonPlanRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
